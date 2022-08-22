9e94d2cc6c0d8e821e415a3187f9044358b648ba
Roman <roman.markanych@gmail.com>
2022-08-23 00:59:42 +0300

    add task to rep

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+const QUERY = 'name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}/${name}?fields=${QUERY}`)
+    .then(response => {
+      if (response.status === 404) {
+        Notify.failure('Oops, there is no country with that name');
+      }
+      return response.json();
+    })
+    .catch(({ name, message }) => console.log(`${name}: ${message}`));
+}
